9029f8f384be69684e231b92f2d748638ef2d1f6
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2026-01-11 18:06:12 +0100

    clean up here and there

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import qrcode
 
-
 def get_user_choices():
     user_input = input('Enter the text or URL for the QR code: ')
-    user_back_color = input('Background color? ')
-    user_fill_color = input('Fill color? ')
+    back_color = input('Background color? ')
+    fill_color = input('Fill color? ')
     file_name = input('Enter the filename: ')
-    
-    return (user_input, user_back_color, user_fill_color, file_name)
+    return user_input, back_color, fill_color, file_name
 
-def generate_QR_code(user_input, user_back_color, user_fill_color):
+def generate_qr_code(user_input, back_color, fill_color):
     qr = qrcode.QRCode(
         version=None,
         box_size=10,
@@ -18,19 +16,17 @@ def generate_QR_code(user_input, user_back_color, user_fill_color):
 
     qr.add_data(user_input)
     qr.make(fit=True)
-    img = qr.make_image(fill_color=user_fill_color, back_color=user_back_color)
-    type(img)
-
+    img = qr.make_image(fill_color=fill_color, back_color=back_color)
     return img
 
-def save_QR_code(image, file_name):
+def save_qr_code(image, file_name):
     image.save(file_name)
     print(f"QR code saved as {file_name}")
 
 def main():
-    user_choices = get_user_choices()
-    generated_image = generate_QR_code(user_choices[0], user_choices[1], user_choices[2])
-    save_QR_code(generated_image, user_choices[3])
+    user_input, back_color, fill_color, file_name = get_user_choices()
+    generated_image = generate_QR_code(user_input, back_color, fill_color)
+    save_QR_code(generated_image, file_name)
 
 if __name__ == "__main__":
     main()
